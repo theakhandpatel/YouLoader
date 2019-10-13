1b162455b0a7c8971280f9f8c8f3345237aa131b
Akhand Patel <akhand3108patel@gmail.com>
2019-10-14 00:29:07 +0530

    refactored the app

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const ytdl = require('ytdl-core');
+const path = require('path');
+
 const app = express();
+
+
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','ejs');
+
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
+
+
+app.get("/",(req,res)=>{
+    res.render("index");
+})
 
 app.get('/download', (req,res) => {
     let URL = req.query.URL;
